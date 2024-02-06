0a63b3770f6bb735abab18b6a807c16f16bc793c
Furkan Beğen <furkanbegen@gmail.com>
2024-02-06 23:48:34 +0300

    Initial

---
+++ src/main/java/com/furkanbegen/springsociallogindemo/SpringSocialLoginDemoApplication.java
@@ -0,0 +1,13 @@
+package com.furkanbegen.springsociallogindemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSocialLoginDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSocialLoginDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/furkanbegen/springsociallogindemo/SpringSocialLoginDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.furkanbegen.springsociallogindemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSocialLoginDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
